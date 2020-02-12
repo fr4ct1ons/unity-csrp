7994c7b3925473bd45079be28145eb4ae9bbf3da
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-12 00:36:46 -0300

    Now drawing geometry

--- CSRP Project/Assets/Custom SRP/Runtime/CameraRenderer.cs
+++ CSRP Project/Assets/Custom SRP/Runtime/CameraRenderer.cs
@@ -2,14 +2,21 @@
 using UnityEngine.Rendering;
 
 public class CameraRenderer {
-
-    ScriptableRenderContext context;
-
-    Camera camera;
+    
+    private const string bufferName = "RenderCamera";
+    private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    
+    private ScriptableRenderContext context;
+    private Camera camera;
+    private  CommandBuffer buffer = new CommandBuffer{name = bufferName};
+    private CullingResults cullingResults;
 
     public void Render (ScriptableRenderContext context, Camera camera) {
         this.context = context;
         this.camera = camera;
+        
+        if(!Cull())
+            return;
 
         Setup();
         DrawVisibleGeometry();
@@ -19,15 +26,49 @@ public class CameraRenderer {
     private void Setup()
     {
         context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
     }
 
     private void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
     }
 
     private void DrawVisibleGeometry()
     {
+        SortingSettings sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
         context.DrawSkybox(camera);
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+    
+    void ExecuteBuffer() 
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+
+        return false;
     }
 }
