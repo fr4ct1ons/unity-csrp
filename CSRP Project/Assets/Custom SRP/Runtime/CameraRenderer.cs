6cc9eef40f2fdf9bd351c2da4b2cfef2b92b9393
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-11 23:57:39 -0300

    Draw skybox

---
+++ CSRP Project/Assets/Custom SRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer {
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    public void Render (ScriptableRenderContext context, Camera camera) {
+        this.context = context;
+        this.camera = camera;
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    private void Setup()
+    {
+        context.SetupCameraProperties(camera);
+    }
+
+    private void Submit()
+    {
+        context.Submit();
+    }
+
+    private void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+}
