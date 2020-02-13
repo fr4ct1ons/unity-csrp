f0a0c359187230d69a857784fbd6eedff9ec5fb1
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-13 20:11:52 -0300

    Adding other files

---
+++ CSRP Project/Assets/Custom SRP/Shaders/LitPass.hlsl
@@ -0,0 +1,33 @@
+﻿#ifndef CUSTOM_LIT_PASS_INCLUDED
+#define CUSTOM_LIT_PASS_INCLUDED
+
+#include "../ShaderLibrary/Common.hlsl"
+
+float4 _BaseColor;
+
+struct Attributes {
+	float3 positionOS : POSITION;
+	float3 normalOS : NORMAL;
+	float2 baseUV : TEXCOORD0;
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings {
+	float4 positionCS : SV_POSITION;
+	float3 normalWS : VAR_NORMAL;
+	float2 baseUV : VAR_BASE_UV;
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+float4 LitPassVertex (float3 positionOS : POSITION) : SV_POSITION 
+{
+    float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    return TransformWorldToHClip(positionWS);
+}
+
+float4 LitPassFragment () : SV_TARGET
+{
+    return _BaseColor;
+}
+
+#endif
