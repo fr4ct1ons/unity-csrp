09a478210a2af1c7a0877d5f51efb75ab01f6839
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-14 14:08:19 -0300

    Added alpha cutoff

--- CSRP Project/Assets/Custom SRP/Shaders/Unlit.shader
+++ CSRP Project/Assets/Custom SRP/Shaders/Unlit.shader
@@ -3,6 +3,9 @@
     {
         _BaseMap("Texture", 2D) = "white" {}
         _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
+        [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 0
+        
         [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1
 		[Enum(UnityEngine.Rendering.BlendMode)] _DstBlend ("Dst Blend", Float) = 0
 		[Enum(Off, 0, On, 1)] _ZWrite ("Z Write", Float) = 1
@@ -16,6 +19,7 @@
             ZWrite [_ZWrite]
             
             HLSLPROGRAM
+            #pragma shader_feature _CLIPPING
             #pragma multi_compile_instancing
 			#pragma vertex UnlitPassVertex
 			#pragma fragment UnlitPassFragment
--- CSRP Project/Assets/Custom SRP/Shaders/UnlitPass.hlsl
+++ CSRP Project/Assets/Custom SRP/Shaders/UnlitPass.hlsl
@@ -9,6 +9,7 @@ SAMPLER(sampler_BaseMap);
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
     UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
 	UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+	UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct Attributes {
@@ -38,7 +39,11 @@ float4 UnlitPassFragment (Varyings input) : SV_TARGET {
 	UNITY_SETUP_INSTANCE_ID(input);
 	float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
 	float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
-	return baseMap * baseColor;
+	float4 base = baseMap * baseColor;
+	#if defined(_CLIPPING)
+		clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+	#endif
+	return base;
 }
 
 #endif
